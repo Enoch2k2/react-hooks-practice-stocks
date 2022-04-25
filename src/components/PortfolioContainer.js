50e5e076efe51170361aa49af1b491ac8bba0bbd
Enoch2k2 <enoch2k2@hotmail.com>
2022-04-25 17:22:57 -0500

    finished the stuffs

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ portfolio, removeStock }) {
+
+  const portfolioArray = portfolio.map(stock => <Stock key={ stock.id } stock={ stock } removeStock={ removeStock } />);
+
   return (
     <div>
       <h2>My Portfolio</h2>
       {
-        //render your portfolio stocks here
+        portfolioArray
       }
     </div>
   );
